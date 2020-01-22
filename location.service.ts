e6546872d331ab18cb2ee462621bb9e6b2a5958e
tresorinho <48109294+tresorinho@users.noreply.github.com>
2020-01-22 13:46:52 +0100

    Add files via upload

---
+++ location.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocationService {
+
+  constructor() { }
+
+
+  registerLocation(location : LocationService){
+    
+  }
+
+  getAllLocations(){
+
+  }
+
+}
